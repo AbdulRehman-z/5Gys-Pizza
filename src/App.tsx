e422845849bd6beecfc1d8c0f46ed3576f82b334
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-26 11:39:44 +0500

    add routing(dynamic)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { menuLoader } from "./features/menu/Menu";
+import CreateOrder, { createOrderAction } from "./features/order/CreateOrder";
+import Order, { orderLoader } from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+import NotFound from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <NotFound />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:id",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <NotFound />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
