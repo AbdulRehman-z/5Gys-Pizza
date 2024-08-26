e97bb67d70474eeaf2c6e37172f4905027778be7
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-26 11:36:20 +0500

    add layout

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,27 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
+import SearchOrder from "../features/order/SearchOrder";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  console.log(navigation.state);
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      <header>
+        <h1>🍕 Pizza Time</h1>
+      </header>
+      <SearchOrder />
+
+      {isLoading && <Loader />}
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
