b27c905f202e1ee1dff4fc824de98f3ded18a861
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-26 13:09:45 +0500

    add errorEl

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ const router = createBrowserRouter([
         path: "/order/new",
         element: <CreateOrder />,
         action: createOrderAction,
+        errorElement: <NotFound />,
       },
       {
         path: "/order/:id",
