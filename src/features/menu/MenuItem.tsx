1f791f088f453b6d8e2f2e93e88fa1e40e17af1e
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-31 14:56:07 +0500

    Add "Add to cart" functionality to MenuItem component

--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -1,4 +1,7 @@
+import { useDispatch } from "react-redux";
+import { Button } from "../../ui/Button";
 import { formatCurrency } from "../../utlis/helpers";
+import { addItem } from "../cart/cartSlice";
 
 type MenuItemProps = {
   pizza: {
@@ -6,22 +9,51 @@ type MenuItemProps = {
     name: string;
     unitPrice: number;
     ingredients: string[];
-    soldOut: boolean;
+    soldOut?: boolean;
     imageUrl: string;
   };
 };
 
 function MenuItem({ pizza }: MenuItemProps) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+    console.log(id);
+  }
+
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+
+          <Button onClick={handleAddToCart} variant="small">
+            Add to cart
+          </Button>
         </div>
       </div>
     </li>
