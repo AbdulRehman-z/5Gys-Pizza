be16a546c98fad3e05df871fc789f3feed4f765f
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-31 14:54:57 +0500

    implement state and use GENERIC bUTTON

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -7,11 +7,13 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import { Button } from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str: string) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -42,53 +44,70 @@ function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
   const formErrors = useActionData();
   const navigation = useNavigation();
+  const username = useSelector((state) => state.username);
 
-  const isSubmiting = navigation.state === "submitting";
-  const isLoading = navigation.state === "loading";
+  console.log(username);
+  const isSubmitting = navigation.state === "submitting";
+  // const isLoading = navigation.state === "loading";
 
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
+      {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label defaultValue={username} className="sm:basis-40">
+            First Name
+          </label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.errors ?? <div>{formErrors?.phone}</div>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
-            value={withPriority.toString()}
-            onChange={(e) => setWithPriority(e.target.checked)}
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmiting || isLoading}>
-            {isSubmiting ? "Placing your order..." : "Order now"}
-          </button>
+          <Button disabled={isSubmitting} variant="primary">
+            {isSubmitting ? "Placing order...." : "Order now"}
+          </Button>
         </div>
       </Form>
     </div>
