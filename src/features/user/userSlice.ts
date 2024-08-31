07bd26117998d47c8d21706062349f6c0cd448c6
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-31 14:53:31 +0500

    add slice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,20 +1,39 @@
-import { getAddress } from "../../services/apiGeocoding";
+import { createSlice } from "@reduxjs/toolkit";
 
-function getPosition() {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
+const initialState = {
+  username: undefined,
+};
 
-async function fetchAddress() {
-  const positionObj = await getPosition();
-  const position = {
-    latitude: (positionObj as GeolocationPosition).coords.latitude,
-    longitude: (positionObj as GeolocationPosition).coords.longitude,
-  };
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {
+    updateUsername: (state, action) => {
+      state.username = action.payload;
+    },
+  },
+});
 
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+export const { updateUsername } = userSlice.actions;
+export default userSlice.reducer;
 
-  return { position, address };
-}
+// import { getAddress } from "../../services/apiGeocoding";
+
+// function getPosition() {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// }
+
+// async function fetchAddress() {
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: (positionObj as GeolocationPosition).coords.latitude,
+//     longitude: (positionObj as GeolocationPosition).coords.longitude,
+//   };
+
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+//   return { position, address };
+// }
