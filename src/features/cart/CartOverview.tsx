45b2327e8ea160f62051c9d6eca211fc8ed941c7
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-09-02 21:38:37 +0500

    add condition

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -11,7 +11,9 @@ function CartOverview() {
   return (
     <div className="text-md flex items-center justify-between bg-stone-800 px-4 py-3 font-semibold text-stone-200 md:px-6 md:text-2xl">
       <p className="space-x-2">
-        <span>{totalCartQuantity} pizzas</span>
+        <span>
+          {totalCartQuantity} {totalCartQuantity > 1 ? "pizzas" : "pizza"}
+        </span>
         <span>${totalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
