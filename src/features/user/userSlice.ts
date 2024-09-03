b2be045ba944fc64e62196a6b10d09f78e9213b1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-09-03 09:27:44 +0500

    Add geolocation functionality to userSlice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,8 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootStateType } from "../../store";
+import { getAddress } from "../../services/apiGeocoding";
+
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const fetchAddress = createAsyncThunk("user/fetchAddress", async () => {
+  const positionObj = await getPosition();
+  const position = {
+    latitude: (positionObj as GeolocationPosition).coords.latitude,
+    longitude: (positionObj as GeolocationPosition).coords.longitude,
+  };
+
+  const addressObj = await getAddress(position);
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+  return { position, address };
+});
+
+// type InitialStateType = {};
 
 const initialState = {
-  username: undefined,
+  username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -13,30 +39,23 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.address = action.payload.address;
+        state.position = action.payload.position;
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = "failed to fetch user location";
+      }),
 });
 
 export const getUsername = (state: RootStateType) => state.user.username;
 
 export const { updateUsername } = userSlice.actions;
 export default userSlice.reducer;
-
-// import { getAddress } from "../../services/apiGeocoding";
-
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
-
-// async function fetchAddress() {
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: (positionObj as GeolocationPosition).coords.latitude,
-//     longitude: (positionObj as GeolocationPosition).coords.longitude,
-//   };
-
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-//   return { position, address };
-// }
