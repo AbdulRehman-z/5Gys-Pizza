e1c0143fa27f94103de6b99fdb392ec20671924c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-09-04 19:31:50 +0500

    Update OrderItem component to display loading state and ingredients

--- src/features/order/OrderItem.tsx
+++ src/features/order/OrderItem.tsx
@@ -8,6 +8,7 @@ type OrderItemProps = {
   };
   isLoadingIngredients?: boolean;
   ingredients?: string[];
+  error?: string;
 };
 
 function OrderItem({
@@ -18,13 +19,16 @@ function OrderItem({
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-600">
+        {isLoadingIngredients ? "loading..." : ingredients?.join(" ,")}
+      </p>
     </li>
   );
 }
